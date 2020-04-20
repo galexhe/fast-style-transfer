a10254d1fbe04f0d431d37bd5beaa472eabad05c
Alex He <Alex.He@ey.com>
2020-04-20 14:37:08 -0700

    Updated the download source

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 mkdir data
 cd data
 # wget http://www.vlfeat.org/matconvnet/models/beta16/imagenet-vgg-verydeep-19.mat
-wget http://fast-style.ewr1.vultrobjects.com/imagenet-vgg-verydeep-19.mat
+wget http://fast.ewr1.vultrobjects.com/imagenet-vgg-verydeep-19.mat
 mkdir bin
 # wget http://msvocds.blob.core.windows.net/coco2014/train2014.zip
-wget https://ewr1.vultrobjects.com/fast-style/train2014.zip
+wget https://fast.ewr1.vultrobjects.com/train2014.zip
 unzip train2014.zip
